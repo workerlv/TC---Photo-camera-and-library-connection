c213a75be7b6b64d075beb9c00343ae5a025c552
Workerlv <arturs280@inbox.lv>
2018-04-24 22:57:59 +0300

    Initial Commit

---
+++ Photo camera and  library connection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo camera and  library connection
+//
+//  Created by Arturs Vitins on 24/04/2018.
+//  Copyright © 2018 Arturs Vitins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
